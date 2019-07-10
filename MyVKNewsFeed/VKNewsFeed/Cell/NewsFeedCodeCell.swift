b9b06305145024076615730e6778380f7391df83
Andrew <incriptos@gmail.com>
2019-07-10 22:54:57 +0300

    add news feed cell from CODE

---
+++ MyVKNewsFeed/VKNewsFeed/Cell/NewsFeedCodeCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsFeedCodeCell.swift
+//  MyVKNewsFeed
+//
+//  Created by Andrew Vashulenko on 7/10/19.
+//  Copyright © 2019 Andrew Vashulenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class NewsFeedCodeCell {
+  
+}
